f35e436ecb05eea027288380b17df9f9117f6eae
Mildred <mildredmrtnz1@gmail.com>
2023-01-13 12:08:00 -0600

    Se hizo el ejercicio 2, no pude borrar el input

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,13 +1,25 @@
-// API
+
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
-/**
- * STEPS:
- *
- * 1. Create a fetchAnswer function and call the API
- * 2. Output the API's response
- * 3. Attach fetchAnswer to an event listener
- * 4. Clear output after 3 seconds
- * 5. Optional: add loading/error states
- *
- */
+// 1. Construye una función fetchAnswer que realice una petición al API https://yesno.wtf/api.
+function fetchAnswer() {
+    fetch('https://yesno.wtf/api')
+      .then(response => response.json())
+      .then(data => {
+// 2. Muestra la respuesta del API en el HTML, en el div con id answer.
+        document.getElementById('answer').innerHTML = data.answer;
+      });
+  }
+  
+// 3. Agrega la función fetchAnswer a un eventListener, para que sea disparado cuando se de click al botón.
+  document.getElementById('button').addEventListener('click', fetchAnswer);
+
+  
+  
+// 4. Limpia la pregunta y la respuesta despues de 5 segundos.
+  setTimeout(() => {
+    document.getElementById('answer').innerHTML = '';
+    document.getElementById('input').innerHTML = '';
+  }, 5000);
+  
+//no se borra la pregunta :(
